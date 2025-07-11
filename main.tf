d73cb1f30fda12ade7746423c247673893007347
aman46788 <amanchauhan1182@gmail.com>
2025-07-11 16:26:08 +0530

    added rg102

--- main.tf
+++ main.tf
@@ -2,7 +2,10 @@ resource "azurerm_resource_group" "RGnarendra7" {
   name     = "rgpradeep7"
   location = "West Europe"
 }
-
+resource "azurerm_resource_group" "RGnarendra8" {
+  name     = "rg102"
+  location = "West Europe"
+}
  resource "azurerm_virtual_network" "vn_router" {
    name = "vnaman"
    address_space = ["10.0.0.0/16"]
