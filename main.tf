ce077b5116422808438ac19f92989ba918900cc5
aman46788 <amanchauhan1182@gmail.com>
2025-07-11 15:59:43 +0530

    adding subnet code

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "RGnarendra7" {
+  name     = "rgpradeep7"
+  location = "West Europe"
+}
+
+ resource "azurerm_virtual_network" "vn_router" {
+   name = "vnaman"
+   address_space = ["10.0.0.0/16"]
+   location = azurerm_resource_group.RGnarendra7
+   resource_group_name      = azurerm_resource_group.RGnarendra7
+
+   subnet {
+    name = "subnet1"
+    address_prefixes  = ["10.0.1.0/16"]
+   }
+ }
